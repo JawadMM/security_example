b5f65fcb2be84f8bcddd011ba02c74c4bdbd0226
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-05-17 16:46:03 +0300

    Added some initial code.

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = 3000;
+
+app = express();
+
+app.get("/secret", (req, res) => {
+  return res.send("Secret is 123");
+});
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
