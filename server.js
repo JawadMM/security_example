425e46638b1e489b71bbd14b04c8e7bfa7a23fa0
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-05-19 02:16:00 +0300

    Set some configurations to integrate with Google OAuth.
    Edited the index.html file and created some endpoints.

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
-const https = require("https")
+const https = require("https");
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
 const PORT = 3000;
 
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallBack(accessToken, refreshToken, profile, done) {
+  console.log("Google Profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack));
+
 app = express();
 
-app.get("/secret", (req, res) => {
+app.use(helmet());
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You need to log in.",
+    });
+  }
+
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Secret is 123");
 });
 
@@ -14,7 +57,14 @@ app.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-https.createServer({}, app).listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-  
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
