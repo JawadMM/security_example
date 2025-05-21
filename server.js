297c6b200ae8e7372c8d9037184718da2a6b35d0
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-05-21 04:39:27 +0300

    Completed authentication and authorization.

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
+
+require("dotenv").config();
 
 const PORT = 3000;
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -26,13 +31,42 @@ function verifyCallBack(accessToken, refreshToken, profile, done) {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack));
 
+// Save session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Read session from the cookie
+passport.deserializeUser((id, done) => {
+  // Could be used to look up a user in the database
+  // The first argument is the same one that the done function takes in the serialize method.
+
+  // User.findById(id).then(user => {
+  //   done(null, user)
+  // })
+
+  done(null, id);
+});
+
 app = express();
 
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+
 app.use(passport.initialize());
+app.use(passport.session()); // Sets up req.user for express
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  console.log(`Current user: ${req.user}`);
+
+  const isLoggedIn = req.isAuthenticated() && req.user;
 
   if (!isLoggedIn) {
     return res.status(401).json({
@@ -43,14 +77,40 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get("/auth/google", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    // scope: ["email", "profle"],
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ],
+  })
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: true,
+  }),
+  (req, res) => {
+    console.log("Google called back.");
+  }
+);
 
-app.get("/auth/logout", (req, res) => {});
+app.get("/auth/logout", (req, res) => {
+  req.logout(); // Remove req.uesr and terminate the existing session
+  return res.redirect("/");
+});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Secret is 123");
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in.");
 });
 
 app.get("/", (req, res) => {
